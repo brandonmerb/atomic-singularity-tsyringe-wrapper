7455c1da269179fa63b55597b20d06b11d99a870
Brandon Erb <brandon.m.erb@gmail.com>
2023-10-25 23:37:04 -0400

    Made this work. Not finished

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
+import { DIToken } from '@atomicdesign/atomic-singularity';
+import { DIProviderTypes } from '@atomicdesign/atomic-singularity';
 import { type AtomicNebulaInterface, createNebula, DependencyInjectionMiddleware } from '@atomicdesign/atomic-singularity'
 import { container } from 'tsyringe';
 
-export const useTSyringe: AtomicNebulaInterface = createNebula({
+export const useTSyringe = createNebula({
   name: "atomic-singularity-tsyringe-wrapper"
 })
   .addPreactivation(() => {
-    DependencyInjectionMiddleware.instance.setProviderFunction((provider, token, type, config) => {
-      container.register(token as any, provider, config);
+    DependencyInjectionMiddleware.instance.setProviderFunction((provider: any, token: DIToken, type: DIProviderTypes, config: any) => {
+      // container.register(token as any, provider, config);
+      const tokenToUse = token ?? provider;
+      if (type) {
+        console.log(type);
+        switch (type) {
+          case 'class':
+            container.register(tokenToUse, {useClass: provider});
+          case 'factory':
+            container.register(tokenToUse, {useFactory: provider});
+          case 'value':
+            container.register(tokenToUse, {useValue: provider});
+        }
+      } else {
+        if (typeof provider === "function") {
+          container.register(tokenToUse, {useClass: provider});
+        } else {
+          container.register(tokenToUse, {useValue: provider});
+        }
+      }
     });
 
-    DependencyInjectionMiddleware.instance.setInjectionFunction((token) => {
-      container.resolve(token);
+    DependencyInjectionMiddleware.instance.setInjectionFunction((token: DIToken) => {
+      return container.resolve(token);
     });
   })
   .build();
